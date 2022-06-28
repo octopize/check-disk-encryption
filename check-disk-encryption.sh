966360d7829f17d2b99ff47a4a9aad8a879b559c
Charles-Axel Dein <120501+charlax@users.noreply.github.com>
2022-06-28 13:57:12 +0200

    fix: invert check on Linux

--- check-disk-encryption.sh
+++ check-disk-encryption.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    if fdesetup status | grep -qi 'off'; then
+    if fdesetup status | grep -qi 'off' > /dev/null; then
         echo "FileVault is off on macOs"
         exit 1
     fi
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if lsblk | grep 'crypt'; then
+    if ! lsblk | grep 'crypt' > /dev/null; then
         echo "There are no encrypted partition on Linux"
         exit 1
     fi
